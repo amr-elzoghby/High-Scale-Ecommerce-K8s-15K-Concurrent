8600c03efe0b5e082e4695cc0d2947312304d45a
3MR <amr01090080@gmail.com>
2026-04-11 09:28:15 +0200

    UPGRADE: improve organization of terraform code

---
+++ web-app/terraform/locals.tf
@@ -0,0 +1,5 @@
+locals {
+  server_name = "web-server"
+  owner       = "3mr-devops"
+  project     = "ecommerce-microservices"
+}
--- web-app/terraform/variables.tf
+++ web-app/terraform/variables.tf
@@ -1,18 +1,11 @@
 variable "instance_type" {
   description = "The type of EC2 instance to use"
-  ami = "ami-0ec10929233384c7f"
-  type = string
-  default = "t3.micro"
-}
-
-locals {
- server_name = "web-server"
- owner = "3mr-devops"
- project = "ecommerce-microservices"
+  type        = string
+  default     = "t3.micro"
 }
 
 variable "db_password" {
   type      = string
-  sensitive = true 
+  sensitive = true
 }
 
